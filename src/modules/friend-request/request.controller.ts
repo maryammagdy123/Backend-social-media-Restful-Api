022239daa0f6ac7ec3318f937110c8b21c917fb3
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2026-05-03 21:54:36 +0300

    feat(request): implement POST endpoint for sending friend requests

--- src/modules/friend-request/request.controller.ts
+++ src/modules/friend-request/request.controller.ts
@@ -1,4 +1,24 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
+import { authenticateUser } from "../../middlewares";
+import requestService from "./request.service";
+import { Types } from "mongoose";
+import { successResponse } from "../../common/response";
 
 const router = Router();
-export default router
+router.post(
+  "/:receiverId",
+  authenticateUser("strict"),
+  async (req: Request, res: Response, next: NextFunction) => {
+    const data = await requestService.sendRequest(
+      req.user._id,
+      new Types.ObjectId(req.params.receiverId as string),
+    );
+    successResponse({
+      res,
+      message: "Request sent successfully!",
+      status: 201,
+      data,
+    });
+  },
+);
+export default router;

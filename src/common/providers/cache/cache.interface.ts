c4fc8241a731e4caecfdc0155d13b29a0808e0ee
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2026-05-09 11:01:37 +0300

    feat(cache): enhance addToSet method documentation with parameter details

--- src/common/providers/cache/cache.interface.ts
+++ src/common/providers/cache/cache.interface.ts
@@ -5,5 +5,9 @@ export interface ICacheProvider extends IAuthRedisProvider {
   set(key: string, value: any, ttl?: number): Promise<void>;
   delete(key: string | string[]): Promise<number>;
   clear(): Promise<string | null>;
+  /**
+   * @param key =>> userId::FCM :[FCM-Token-1,FCM-Token-2,....]
+   * @param value =>> FCM-Token
+   */
   addToSet(key: string, value: string): Promise<void>;
 }

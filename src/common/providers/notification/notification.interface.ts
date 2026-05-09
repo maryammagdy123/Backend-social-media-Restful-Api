b9142caf0afc42d5f82ea8582987672c170516d0
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2026-05-09 12:14:40 +0300

    feat: add sendMultiple method to notification provider interface

--- src/common/providers/notification/notification.interface.ts
+++ src/common/providers/notification/notification.interface.ts
@@ -1,4 +1,8 @@
 export interface INotificationProvider {
   // token like fcm (firebase cloud messaging)
   send(token: string, data: { title: string; body: string }): Promise<void>;
+  sendMultiple(
+    tokens: string[],
+    data: { title: string; body: string },
+  ): Promise<void>;
 }

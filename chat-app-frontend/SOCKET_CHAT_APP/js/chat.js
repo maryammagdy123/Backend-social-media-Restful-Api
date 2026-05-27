25d9fc4dfc0fc73d181ad3743d8d73dadc16799a
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2026-05-27 22:52:10 +0300

    feat(chat-app):integrate chat app with FE

---
+++ chat-app-frontend/SOCKET_CHAT_APP/js/chat.js
@@ -0,0 +1,371 @@
+const baseURL = "http://localhost:3000";
+//images links
+let avatar = "./avatar/Avatar-No-Background.png";
+let meImage = "./avatar/Avatar-No-Background.png";
+let friendImage = "./avatar/Avatar-No-Background.png";
+
+const token = localStorage.getItem("token");
+const bearerToken = `Bearer ${token}`;
+let globalProfile = {};
+const headers = {
+  "Content-Type": "application/json; charset=UTF-8",
+  authorization: bearerToken,
+};
+const socket = io(baseURL, {
+  auth: { token },
+}); // socket call method [connect] >> emit event connection to the backend
+
+socket.on("likePost", (data) => {
+  console.log({ likePost: data });
+});
+
+socket.on("connect_error", (err) => {
+  console.log("connect_error:", err.message);
+});
+
+socket.on("custom_error", (err) => {
+  console.log("custom_error:", err.message);
+});
+
+socket.emit("sayHi", { name: "FROM FE TO BE" }, (response) => {
+  console.log({ response });
+});
+socket.on("offline_user", (data) => {
+  console.log({ data });
+});
+
+// // // collect messageInfo
+function sendMessage(sendTo, type) {
+  console.log({ sendTo, type });
+
+  if (type == "ovo") {
+    const data = {
+      content: $("#messageBody").val(),
+      sendTo,
+    };
+    console.log({ data });
+
+    socket.emit("sendMessage", data);
+  } else if (type == "group") {
+    const data = {
+      content: $("#messageBody").val(),
+      groupId: sendTo,
+    };
+    socket.emit("sendGroupMessage", data);
+  }
+}
+
+// // // // // //sendCompleted
+socket.on("successMessage", (data) => {
+  const onclickAttr = document
+    .getElementById("sendMessage")
+    .getAttribute("onclick");
+  const [base, currentOpenedChat] =
+    onclickAttr?.match(/sendMessage\('([^']+)'/) || [];
+
+  const { content, sendTo } = data;
+  console.log({ sendTo, currentOpenedChat });
+
+  if (sendTo == currentOpenedChat) {
+    const div = document.createElement("div");
+
+    div.className = "me text-end p-2";
+    div.dir = "rtl";
+    const imagePath = globalProfile.profilePicture
+      ? `${baseURL}/uploads/${globalProfile.profilePicture}`
+      : avatar;
+    div.innerHTML = `
+    <img class="chatImage" src="${imagePath}" alt="" srcset="">
+    <span class="mx-2">${content}</span>
+    `;
+    document.getElementById("messageList").appendChild(div);
+    $(".noResult").hide();
+    $("#messageBody").val("");
+  }
+});
+
+// // // // // // // // // //receiveMessage
+socket.on("newMessage", (data) => {
+  console.log({ RM: data });
+  const { content, from, groupId } = data;
+  console.log({ from });
+
+  let imagePath = avatar;
+  if (from?.profilePicture) {
+    imagePath = `${baseURL}/uploads/${from.profilePicture}`;
+  }
+  const onclickAttr = document
+    .getElementById("sendMessage")
+    .getAttribute("onclick");
+  const [base, currentOpenedChat] =
+    onclickAttr?.match(/sendMessage\('([^']+)'/) || [];
+  console.log({ currentOpenedChat });
+  console.log({ onclickAttr, currentOpenedChat });
+
+  if (
+    (!groupId && currentOpenedChat === from._id) ||
+    (groupId && currentOpenedChat === groupId)
+  ) {
+    if (from?._id.toString() != globalProfile._id.toString()) {
+      const div = document.createElement("div");
+      div.className = "myFriend p-2";
+      div.dir = "ltr";
+      div.innerHTML = `
+    <img class="chatImage" src="${imagePath}" alt="" srcset="">
+    <span class="mx-2">${content}</span>
+    `;
+      document.getElementById("messageList").appendChild(div);
+    }
+  } else {
+    if (groupId) {
+      $(`#g_${groupId}`).show();
+    } else {
+      $(`#c_${from._id}`).show();
+    }
+    const audio = document.getElementById("notifyTone");
+    audio.currentTime = 0; // restart from beginning
+    audio.play().catch((err) => console.log("Audio play blocked:", err));
+  }
+});
+
+// // // ******************************************************************** Show chat conversation
+function showData(sendTo, chat) {
+  document
+    .getElementById("sendMessage")
+    .setAttribute("onclick", `sendMessage('${sendTo}' , "ovo")`);
+
+  document.getElementById("messageList").innerHTML = "";
+  if (chat?.messages?.length) {
+    $(".noResult").hide();
+    for (const message of chat.messages) {
+      if (message.sender.toString() == globalProfile._id.toString()) {
+        const div = document.createElement("div");
+        div.className = "me text-end p-2";
+        div.dir = "rtl";
+        div.innerHTML = `
+                <img class="chatImage" src="${meImage}" alt="" srcset="">
+                <span class="mx-2">${message.content}</span>
+                `;
+        document.getElementById("messageList").appendChild(div);
+      } else {
+        const div = document.createElement("div");
+        div.className = "myFriend p-2";
+        div.dir = "ltr";
+        div.innerHTML = `
+                <img class="chatImage" src="${friendImage}" alt="" srcset="">
+                <span class="mx-2">${message.content}</span>
+                `;
+        document.getElementById("messageList").appendChild(div);
+      }
+    }
+  } else {
+    const div = document.createElement("div");
+
+    div.className = "noResult text-center  p-2";
+    div.dir = "ltr";
+    div.innerHTML = `
+        <span class="mx-2">Say Hi to start the conversation.</span>
+        `;
+    document.getElementById("messageList").appendChild(div);
+  }
+
+  $(`#c_${sendTo}`).hide();
+}
+
+// // // // // // // // //get chat conversation between 2 users and pass it to ShowData fun
+function displayChatUser(userId) {
+  axios({
+    method: "get",
+    url: `${baseURL}/chat/${userId}`,
+    headers,
+  })
+    .then(function (response) {
+      const { chat, messages } = response.data?.data;
+
+      if (chat) {
+        if (
+          chat.participants[0]._id.toString() == globalProfile._id.toString()
+        ) {
+          meImage = chat.participants[0].profilePicture
+            ? `${baseURL}/uploads/${chat.participants[0].profilePicture}`
+            : avatar;
+          friendImage = chat.participants[1].profilePicture
+            ? `${baseURL}/uploads/${chat.participants[1].profilePicture}`
+            : avatar;
+        } else {
+          meImage = chat.participants[1].profilePicture
+            ? `${baseURL}/uploads/${chat.participants[1].profilePicture}`
+            : avatar;
+          friendImage = chat.participants[0].profilePicture
+            ? `${baseURL}/uploads/${chat.participants[0].profilePicture}`
+            : avatar;
+        }
+        chat.messages = messages;
+        showData(userId, chat);
+      } else {
+        showData(userId, 0);
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+      console.log({ status: error.status });
+      if (error.status) {
+        showData(userId, 0);
+      } else {
+        alert("Ops something went wrong");
+      }
+    });
+}
+
+// // // // ********************************************************************
+// // // // // ******************************************************************** Show  group chat conversation
+function showGroupData(sendTo, chat) {
+  document
+    .getElementById("sendMessage")
+    .setAttribute("onclick", `sendMessage('${sendTo}' , "group")`);
+
+  document.getElementById("messageList").innerHTML = "";
+  if (chat.messages?.length) {
+    $(".noResult").hide();
+    console.log(chat.messages);
+
+    for (const message of chat.messages) {
+      if (message.createdBy?._id?.toString() == globalProfile._id?.toString()) {
+        const div = document.createElement("div");
+        div.className = "me text-end p-2";
+        div.dir = "rtl";
+        div.innerHTML = `
+                <img class="chatImage" src="${meImage}" alt="" srcset="">
+                <span class="mx-2">${message.content}</span>
+                `;
+        document.getElementById("messageList").appendChild(div);
+      } else {
+        const div = document.createElement("div");
+        div.className = "myFriend p-2";
+        div.dir = "ltr";
+        const friendImage = message.createdBy.profilePicture
+          ? `${baseURL}/uploads/${message.createdBy.profilePicture}`
+          : avatar;
+        div.innerHTML = `
+                <img class="chatImage" src="${friendImage}" alt="" srcset="">
+                <span class="mx-2">${message.content}</span>
+                `;
+        document.getElementById("messageList").appendChild(div);
+      }
+    }
+  } else {
+    const div = document.createElement("div");
+
+    div.className = "noResult text-center  p-2";
+    div.dir = "ltr";
+    div.innerHTML = `
+        <span class="mx-2">Say Hi to start the conversation.</span>
+        `;
+    document.getElementById("messageList").appendChild(div);
+  }
+  $(`#g_${sendTo}`).hide();
+}
+// // // // // // // ********************************************************************
+function displayGroupChat(groupId) {
+  console.log({ groupId });
+  axios({
+    method: "get",
+    url: `${baseURL}/chat/group/${groupId}`,
+    headers,
+  })
+    .then(function (response) {
+      const { chat } = response.data?.data;
+      console.log({ chat });
+      if (chat) {
+        meImage = globalProfile.profilePicture
+          ? `${baseURL}/uploads/${globalProfile.profilePicture}`
+          : avatar;
+        showGroupData(groupId, chat);
+      } else {
+        showGroupData(groupId, 0);
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+      console.log({ status: error.status });
+      if (error.status) {
+        showGroupData(groupId, 0);
+      } else {
+        alert("Ops something went wrong");
+      }
+    });
+}
+// // // // ==============================================================================================
+
+// // ********************************************************* Show Users list
+// Display Users
+function getUserData() {
+  axios({
+    method: "get",
+    url: `${baseURL}/user`,
+    headers,
+  })
+    .then(function (response) {
+      const { user, groups, friends } = response.data?.data;
+      globalProfile = user;
+      let imagePath = avatar;
+      if (user.profilePicture) {
+        imagePath = `${baseURL}/uploads/${user.profilePicture}`;
+      }
+      document.getElementById("profileImage").src = imagePath;
+      document.getElementById("userName").innerHTML = `${user.userName}`;
+      showUsersData(friends, user.userName);
+      showGroupList(groups);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+// Show friends list
+function showUsersData(friends = [], userName) {
+  let cartonna = ``;
+  for (let i = 0; i < friends.length; i++) {
+    let imagePath = avatar;
+    if (friends[i].profilePicture) {
+      imagePath = `${baseURL}/uploads/${friends[i].profilePicture}`;
+    }
+    cartonna += `
+        <div onclick="displayChatUser('${friends[i]._id}')" class="chatUser my-2">
+        <img class="chatImage" src="${imagePath}" alt="" srcset="">
+        <span class="ps-2">${friends[i].user.userName == userName ? friends[i].friend.userName : friends[i].user.userName}</span>
+        
+        <span id="${"c_" + friends[i]._id}" class="ps-2 closeSpan">
+           🟢
+        </span>
+    </div>
+        
+        `;
+  }
+
+  document.getElementById("chatUsers").innerHTML = cartonna;
+}
+
+// // // // // Show groups list
+function showGroupList(groups = []) {
+  let cartonna = ``;
+  for (let i = 0; i < groups.length; i++) {
+    let imagePath = avatar;
+    if (groups[i].group_image) {
+      imagePath = `${baseURL}/uploads/${groups[i].group_image}`;
+    }
+    cartonna += `
+        <div onclick="displayGroupChat('${groups[i]._id}')" class="chatUser my-2">
+        <img class="chatImage" src="${imagePath}" alt="" srcset="">
+        <span class="ps-2">${groups[i].group}</span>
+           <span id="${"g_" + groups[i]._id}" class="ps-2 closeSpan">
+           🟢
+        </span>
+    </div>
+
+        `;
+    socket.emit("join_room", { roomId: groups[i].roomId });
+  }
+
+  document.getElementById("chatGroups").innerHTML = cartonna;
+}
+getUserData();
---
+++ chat-app-frontend/SOCKET_CHAT_APP/js/index.js
@@ -0,0 +1,28 @@
+const baseURL = "http://localhost:3000";
+
+$("#login").click(() => {
+  const email = $("#email").val();
+  const password = $("#password").val();
+  const data = {
+    email,
+    password,
+  };
+  axios({
+    method: "post",
+    url: `${baseURL}/auth/login`,
+    data: data,
+    headers: { "Content-Type": "application/json; charset=UTF-8" },
+  })
+    .then(function (response) {
+      const { message, data, success } = response.data;
+      if (success) {
+        localStorage.setItem("token", data.accessToken);
+        window.location.href = "chat.html";
+      } else {
+        alert("In-valid email or password");
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});

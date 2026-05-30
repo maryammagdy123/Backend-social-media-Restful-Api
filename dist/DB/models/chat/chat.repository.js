a999acc257685fff174ae7a6039f8f728ad65588
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2026-05-30 23:19:39 +0300

    feat(chat): create ChatRepository class extending AbstractDBRepository

---
+++ dist/DB/models/chat/chat.repository.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ChatRepository = void 0;
+const db_repository_1 = require("../../repository/db.repository");
+const chat_model_1 = require("./chat.model");
+class ChatRepository extends db_repository_1.AbstractDBRepository {
+    constructor() {
+        super(chat_model_1.ChatModel);
+    }
+}
+exports.ChatRepository = ChatRepository;
---
+++ src/DB/models/chat/chat.repository.ts
@@ -0,0 +1,9 @@
+import { IChat } from "../../../common";
+import { AbstractDBRepository } from "../../repository/db.repository";
+import { ChatModel } from "./chat.model";
+
+export class ChatRepository extends AbstractDBRepository<IChat> {
+  constructor() {
+    super(ChatModel);
+  }
+}
